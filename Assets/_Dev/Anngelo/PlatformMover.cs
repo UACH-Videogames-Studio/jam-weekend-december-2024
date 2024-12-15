272821c6228990723ddbcb19a18f875db8342315
Ángel Ortiz <anngelo.omz@gmail.com>
2024-12-15 13:36:56 -0600

    Add advance to level2

--- Assets/_Dev/Anngelo/PlatformMover.cs
+++ Assets/_Dev/Anngelo/PlatformMover.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PlatformMover : MonoBehaviour
 {
-    public Vector2 startPoint;  
-    public Vector2 endPoint;    
-    public float speed = 2f; 
+    public Vector2 startPoint;
+    public Vector2 endPoint;
+    public float speed = 2f;
     private bool movingToEnd = true;
-    
+
     void Start()
     {
         transform.position = startPoint;
@@ -34,6 +34,24 @@ public class PlatformMover : MonoBehaviour
             transform.position = Vector2.MoveTowards(transform.position, startPoint, speed * Time.deltaTime);
             if (Vector2.Distance(transform.position, startPoint) < 0.1f) //Ya llego al destino
                 movingToEnd = true;  // Cambiar de direccion
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //Vuelve al player hijo de la plataforma para que se desplacen al mismo tiempo
+            collision.transform.SetParent(this.transform);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Deja de ser hijo de la plataforma y se mueve independiente de nuevo
+            collision.transform.SetParent(null);
         }
     }
 }
